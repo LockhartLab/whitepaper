90893c61ae6fd13a3ddc1ed17feb160717751f9c
doclockh <chris@lockhartlab.org>
2020-01-21 15:40:47 -0800

    Changing chart formatting

--- whitepaper/google/google.py
+++ whitepaper/google/google.py
@@ -27,6 +27,8 @@ this.cache = []
 
 # TODO: split this off into its own package
 # TODO should this be called GVizChart?
+# TODO add method to create from dataframe
+# TODO handle when x is a date (needs to be new Date(...))
 class GoogleChart:
     """
     Generate the HTML for charts with Google Visualization API
@@ -72,7 +74,7 @@ class GoogleChart:
 
         # Create DataFrame for x, y and combine with class data
         _data = pd.DataFrame({'x': x, label: y})
-        self._data = self._data.merge(_data, how='outer', on='x')
+        self._data = self._data.merge(_data, how='outer', on='x').fillna(0)
 
         # Save information
         self._labels.append(label)
@@ -80,7 +82,7 @@ class GoogleChart:
         self._styles.append(style)
 
     # Show the Google Chart
-    def show(self, render_loader=True):
+    def show(self, render_loader=True, width='100%', height='500px'):
         # Render loader if necessary
         output = '' if not render_loader else """
             <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -106,12 +108,12 @@ class GoogleChart:
 
         # Formatting for x axis
         output += """
-            vAxis: {{title: "{x_title}"}},
+            hAxis: {{title: "{x_title}"}},
         """.format(x_title=self._x_title if self._x_title is not None else '')
 
         # Formatting for y axis
         output += """
-            hAxis: {{title: "{y_title}"}},
+            vAxis: {{title: "{y_title}"}},
         """.format(y_title=self._y_title if self._y_title is not None else '')
 
         # Legend
@@ -120,26 +122,25 @@ class GoogleChart:
         """.format(legend=self._legend if self._legend is not None else 'none')
 
         # Close out script
-        # TODO add ability to adjust width, height
         output += """
             }};
             var chart = new google.visualization.LineChart(document.getElementById("{id}"));
             chart.draw(data, options);
             }}
             </script>
-            <div id="{id}" style="width: 100%;"></div>
-        """.format(id=self._id)
+            <div id="{id}" style="width: {width}; height: {height};"></div>
+        """.format(id=self._id, width=width, height=height)
 
         # Return
         return output
 
-    # Set x label
-    def xlabel(self, label):
-        self._x_title = label
+    # Set x title
+    def xtitle(self, label):
+        self._x_title = title
 
-    # Set y label
-    def ylabel(self, label):
-        self._y_title = label
+    # Set y title
+    def ytitle(self, title):
+        self._y_title = title
 
 
 class GoogleTable:
